346a50113c87f2361df17ce5d9850c1b77ae81b5
wernere-eng <40810186+wernere-eng@users.noreply.github.com>
2019-05-02 10:23:35 +1000

    pandas tutorial

--- Practice/numpy-arrays.py
+++ Practice/numpy-arrays.py
@@ -11,3 +11,12 @@ my_array = np.array([[1,2,3,4], [5,6,7,8]], dtype=np.int64)
 
 print(my_array)
 
+ones_array = np.ones((3,4))
+print(ones_array )
+
+zero_array = np.zeros((2,3,4),dtype=np.int64)
+print(zero_array)
+
+empty_array = np.empty((4,4) ,dtype=np.int32)
+print(empty_array)
+
---
+++ Practice/pandas-intro.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu May  2 10:13:52 2019
+
+@author: werne
+"""
+
+import numpy as np
+import pandas  as pd
+
+pd.read_csv('zoo.csv' , delimiter = ',' )
+
